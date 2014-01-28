b237351184de74bbe8613825f72aa6a14c21cdba
Souheil CHELFOUH <trollfot@gmail.com>
2014-01-28 17:19:56 +0100

    Removed unused imports

--- src/cromlech/sqlalchemy/__init__.py
+++ src/cromlech/sqlalchemy/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from cromlech.sqlalchemy.components import (
-    create_engine, register_engine, query_engine, create_and_register_engine)
-
-from cromlech.sqlalchemy.controlled import (
-    SQLAlchemySession, SharedSQLAlchemySession, get_session)
+from .components import create_engine
+from .controlled import SQLAlchemySession, SharedSQLAlchemySession, get_session
